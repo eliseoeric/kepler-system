572c64e06330a93499635c2f531b765437f509d4
Eric Eliseo <eric.eliseo@gmail.com>
2025-08-20 14:59:39 -0500

    chore: add storybook docs
    This commit updates the existing docs project to use Storybook and
    removes the Nextjs dependencies.

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,50 @@
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+
+import { join, dirname, resolve } from "path"
+
+/**
+* This function is used to resolve the absolute path of a package.
+* It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
+
+const config: StorybookConfig = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-docs'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-vitest")
+  ],
+  "framework": {
+    "name": getAbsolutePath("@storybook/nextjs-vite"),
+    "options": {}
+  },
+
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return {
+      ...config,
+      define: { "process.env": {} },
+      resolve: {
+        alias: [
+          {
+            find: "ui",
+            replacement: resolve(__dirname, "../../../packages/ui/"),
+          },
+        ],
+      },
+    };
+  },
+
+  docs: {
+    autodocs: true,
+  },
+};
+export default config;
